2a227ffe5c5c6883f636de2dcd2d30be50463df6
mwkang <augustman@knou.ac.kr>
2021-04-22 23:51:08 +0900

    Generic test code addition.

--- src/generic/Generic1.java
+++ src/generic/Generic1.java
@@ -2,14 +2,32 @@ package generic;
 
 public class Generic1 {
     public static void main(String[] args) {
-        Toy toy = new Toy();
-        System.out.println(toy);
+        Box<Toy> box = new Box<>();
+        BoxHandler.inBox(box, new Toy());
+        BoxHandler.outBox(box);
     }
+}
+
+class Box<T> {
+    private T ob;
+    public void set(T o) { this.ob = o; }
+    public T get() { return this.ob; }
 }
 
 class Toy {
     @Override
     public String toString() {
         return "I am a Toy";
+    }
+}
+
+class BoxHandler {
+    public static void outBox(Box<Toy> box) {
+        Toy t = box.get();
+        System.out.println(t);
+    }
+
+    public static void inBox(Box<Toy> box, Toy n) {
+        box.set(n);
     }
 }
