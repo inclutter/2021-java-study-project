b1187cb25120986373414f98dfef5e03911db721
mwkang <augustman@knou.ac.kr>
2021-04-23 09:58:59 +0900

    About generic code and comment addition.

--- src/generic/Generic1.java
+++ src/generic/Generic1.java
@@ -5,12 +5,16 @@ public class Generic1 {
         Box<Toy> box = new Box<>();
         BoxHandler.inBox(box, new Toy());
         BoxHandler.outBox(box);
+        ToStringTest toStringTest = new ToStringTest();
+        System.out.println(toStringTest);
     }
 }
 
 class Box<T> {
     private T ob;
     public void set(T o) { this.ob = o; }
+    // 객체를 리턴할 경우 만약 @Override toString을 했다면
+    // 해당 객체 주소가 리턴되는게 아니고 overriding한 새 toString 메소드의 결과가 리턴된다.
     public T get() { return this.ob; }
 }
 
@@ -29,5 +33,12 @@ class BoxHandler {
 
     public static void inBox(Box<Toy> box, Toy n) {
         box.set(n);
+    }
+}
+
+class ToStringTest {
+    @Override
+    public String toString() {
+        return "To string test.";
     }
 }
