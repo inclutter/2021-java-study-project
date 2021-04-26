3a272052f0616700f568bfe008b058ad4506dc28
mwkang <augustman@knou.ac.kr>
2021-04-26 12:32:38 +0900

    Have modified GenericInInheritance class.

--- src/generic2/GenericInheritance.java
+++ src/generic2/GenericInheritance.java
@@ -2,8 +2,10 @@ package generic2;
 
 public class GenericInheritance {
     public static void main(String[] args) {
-        SteelBox<Integer> iBox = new SteelBox<Integer>(7959);
-        SteelBox<String> sBox = new SteelBox<String>("Simple");
+        // 아래 SteelBox class에서 Box 클래스에 있는 ob 변수의 값을 저장했기 때문에 다음과 같이
+        // Box클래스에 SteellBox 인스턴스를 전달해도 해당 값을 정상적으로 getting 할 수 있게 되었다.
+        Box<Integer> iBox = new SteelBox<Integer>(7959);
+        Box<String> sBox = new SteelBox<String>("Simple");
         System.out.println(iBox.getOb());
         System.out.println(sBox.getOb());
     }
@@ -21,7 +23,12 @@ class Box<T> {
     }
 }
 
+/**
+ * Box class를 상속한다
+ * @param <T>
+ */
 class SteelBox<T> extends Box<T> {
+    // 상속한 박스 class에 있는 ob 인자에 파라미터 값을 전달한다.
     public SteelBox(T o) {
         ob = o;
     }
