c4c8f6cb4890456af0ee35565d87c0fd200b933b
mwkang <augustman@knou.ac.kr>
2021-05-21 09:41:33 +0900

    I have added about HashExample1

---
+++ src/collection_framework/HashExample1.java
@@ -0,0 +1,14 @@
+package collection_framework;
+
+public class HashExample1 {
+}
+
+class Person {
+    private String name;
+    private String age;
+
+    public Person(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+}
