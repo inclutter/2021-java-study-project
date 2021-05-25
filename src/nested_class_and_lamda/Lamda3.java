7b196cb090396233d328a8bb70f8d0af0c7e5b74
mwkang <augustman@knou.ac.kr>
2021-05-25 12:36:13 +0900

    I modfied lamda example class.

--- src/nested_class_and_lamda/Lamda3.java
+++ src/nested_class_and_lamda/Lamda3.java
@@ -2,7 +2,9 @@ package nested_class_and_lamda;
 
 public class Lamda3 {
     public static void main(String[] args) {
-        Printable prn = (System.out::println);
+        Printable prn = (s -> {
+            String p = s;
+            System.out.println(p);});
         prn.print("ss");
     }
 }
